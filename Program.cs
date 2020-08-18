30f0a26c906020a9372aceb553a23b3b4e5baef2
alexalok <work@alexalok.me>
2020-08-18 13:13:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IISLogDeleter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
